cbb1f4a55ab9bfa56b624e9b0d6f71b6a7f9a8ec
Aaron Dennis <AMDennis1987@gmail.com>
2018-10-03 20:24:53 -0700

    loops and if

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello everyone!");
+var x = 11;
+
+if (x === 0) {
+    console.log(x, 'is zero');
+} else if (x % 2 === 0) {
+    console.log(x, 'is even');
+} else {
+    console.log(x, 'is odd');
+}
+
+/*for(var i = 0; i <= 10; i++) {
+    var y = i * 2;
+    if(y < 10) {
+        console.log('the number doubled is ', i);
+    }
+} */
+
+for(var i = 0; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i + 'is even');
+    }
+}
